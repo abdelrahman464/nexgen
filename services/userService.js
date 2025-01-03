fbdbcb25c668f3c5c16f131b0929e11ebca316b1
Abd_EL Rahman Mohamed <apdomedo6@gmail.com>
2025-01-03 20:23:13 +0200

    fixing

--- services/userService.js
+++ services/userService.js
@@ -310,17 +310,55 @@ exports.getUsers = factory.getALl(User, 'User');
 //@access public
 exports.getUser = async (req, res, next) => {
   try {
+    //1- check if token exists, if exist get it
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    if (!token) {
+      return next(new ApiError('you are not login,please login first', 401));
+    }
+    //2- verify token (no change happens,expired token)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    // 3- Check if user exists
+    const currentUser = await User.findById(decoded.userId);
+    if (!currentUser) {
+      return next(new ApiError('User no longer exists', 401));
+    }
+    //4-check if user changed password after token generated
+    if (currentUser.passwordChangedAt) {
+      //convert data to timestamp by =>getTime()
+      const passwordChangedTimestamp = parseInt(
+        currentUser.passwordChangedAt.getTime() / 1000,
+        10,
+      );
+      //it mean password changer after token generated
+      if (passwordChangedTimestamp > decoded.iat) {
+        return next(
+          new ApiError(
+            'user recently changed his password,please login again',
+            401,
+          ),
+        );
+      }
+    }
+
+    //----------------------
     let user = {};
-    if (req.user.role === 'admin') {
-      user = await User.findById(req.params.id);
+    if (currentUser.role === 'admin') {
+      user = await User.findById(currentUser._id);
     } else {
-      user = await User.findById(req.params.id).select(
+      user = await User.findById(currentUser._id).select(
         'name email profileImg authToReview coverImg role timeSpent isMarketer isInstructor isCustomerService startMarketing idNumber phone country idVerification note',
       );
     }
     if (!user) {
       return next(
-        new ApiError(`No document found with this id ${req.params.id}`, 404),
+        new ApiError(`No document found with this id ${currentUser._id}`, 404),
       );
     }
     res.status(200).json({ data: user });
