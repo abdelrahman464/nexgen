f193fce3ce858129ad8029439cbdc5418a29c483
Abd_EL Rahman Mohamed <apdomedo6@gmail.com>
2026-03-19 17:57:26 +0200

    adding

--- models/articalModel.js
+++ models/articalModel.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const ArticalSchema = new mongoose.Schema(
   {
     author: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: 'User',
     },
     title: {
       type: String,
-      required: [true, "Article title required"],
+      required: [true, 'Article title required'],
       trim: true,
       minlength: 2,
       i18n: true,
@@ -20,14 +20,14 @@ const ArticalSchema = new mongoose.Schema(
     },
     description: {
       type: String,
-      required: [true, "Article description required"],
+      required: [true, 'Article description required'],
       trim: true,
       minlength: 10,
       i18n: true,
     },
     content: {
       type: String,
-      required: [true, "Article content required"],
+      required: [true, 'Article content required'],
       i18n: true,
     },
     date: {
@@ -46,15 +46,28 @@ const ArticalSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ["active", "pending"],
-      default: "pending",
+      enum: ['active', 'pending'],
+      default: 'pending',
+    },
+    // SEO fields
+    metaTitle: {
+      type: String,
+      i18n: true,
+    },
+    metaDescription: {
+      type: String,
+      i18n: true,
+    },
+    keywords: {
+      type: String,
+      i18n: true,
     },
   },
   {
     timestamps: true,
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-  }
+  },
 );
 
 const setImageURL = (doc) => {
@@ -64,16 +77,16 @@ const setImageURL = (doc) => {
   }
 };
 
-ArticalSchema.post("init", (doc) => {
+ArticalSchema.post('init', (doc) => {
   setImageURL(doc);
 });
-ArticalSchema.post("save", (doc) => {
+ArticalSchema.post('save', (doc) => {
   setImageURL(doc);
 });
 
 ArticalSchema.pre(/^find/, function (next) {
-  this.populate({ path: "author", select: "_id name email profileImg" });
+  this.populate({ path: 'author', select: '_id name email profileImg' });
   next();
 });
 
-module.exports = mongoose.model("Artical", ArticalSchema);
+module.exports = mongoose.model('Artical', ArticalSchema);
